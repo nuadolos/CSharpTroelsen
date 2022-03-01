e4034c48095892064139be8dab1a5666591bf154
nuadolos <2nuadolos1@gmail.com>
2022-03-01 20:53:17 +0300

    Create a service Windows

---
+++ WCF/BenWindowsServiceHost/BenWindowsServiceHost/BenWinService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using BenTalkingServiceLib;
+using System.ServiceModel;
+
+namespace BenWindowsServiceHost
+{
+    public partial class BenWinService : ServiceBase
+    {
+        private ServiceHost benHost;
+
+        public BenWinService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //Если хост не был закрыт, то закрыть его
+            benHost?.Close();
+
+            ////Создание хоста и ABC
+            //benHost = new ServiceHost(typeof(BenTalkingService));
+            //Uri address = new Uri("http://localhost:8080/BenTalkingService");
+            //WSHttpBinding binding = new WSHttpBinding();
+            //Type contract = typeof(IBenTalking);
+
+            ////Добавление конечной точки
+            //benHost.AddServiceEndpoint(contract, binding, address);
+
+            //Создание хоста с указанием адреса
+            benHost = new ServiceHost(typeof(BenTalkingService), new Uri("http://localhost:8080/BenTalkingService"));
+
+            //Использование стандартной конечной точки
+            benHost.AddDefaultEndpoints();
+
+            //Открытие хоста
+            benHost.Open();
+
+            /*Для того, чтобы установить службу Windows необходимо:
+             * 1) Во вкладке "Вид" выбрать "Терминал"
+             * 2) Прописать команду "installutil BenWindowsServiceHost.exe"
+             * P.S. нужны права администратора
+             * 
+             * Чтобы удалить данную службу необходимо:
+             * 1) Открыть командную строку с правами администратора
+             * 2) Прописать команду "sc delete (Название службы - BenService)"
+             */
+        }
+
+        protected override void OnStop()
+        {
+            //Остановка хоста
+            benHost?.Close();
+        }
+    }
+}
---
+++ WCF/BenWindowsServiceHost/BenWindowsServiceHost/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace BenWindowsServiceHost
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BenProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.BenInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // BenProcessInstaller
+            // 
+            this.BenProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.BenProcessInstaller.Password = null;
+            this.BenProcessInstaller.Username = null;
+            // 
+            // BenInstaller
+            // 
+            this.BenInstaller.Description = "Ben Talking now in the service Windows";
+            this.BenInstaller.ServiceName = "BenService";
+            this.BenInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.BenProcessInstaller,
+            this.BenInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller BenProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller BenInstaller;
+    }
+}

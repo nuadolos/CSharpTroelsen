d11a2c52e2a49373ead80572ac45542cae22e688
nuadolos <2nuadolos1@gmail.com>
2022-04-03 16:45:41 +0300

    Added content resources

---
+++ MaterialResources/TestCIL/HelloProgram.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows.Forms;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MessageBox.Show("govno");
+        Console.WriteLine("Hello CIL code!");
+        string lox = Console.ReadLine();
+        global::System.Console.WriteLine(lox);
+        Console.ReadKey();
+    }
+}
